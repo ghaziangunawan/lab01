ae5a028acfb962437a6fa404db74eaaa2c89d7b9
Ghazian Gunawan <ghazian@Ghazians-MacBook-Pro.local>
2022-09-08 11:14:06 +0700

    update

--- project_django/urls.py
+++ project_django/urls.py
@@ -19,4 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('example_app.urls')),
+    path('wishlist/', include('wishlist.urls')),
+
+
 ]
---
+++ wishlist/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+from django.db import models
+
+
+
+# Create your models here.
+class ItemWishlist(models.Model):
+    item_name = models.CharField(max_length=50)
+    item_price = models.IntegerField()
+    description = models.TextField()
