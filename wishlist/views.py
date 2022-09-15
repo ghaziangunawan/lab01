d73b3a7cb91ab4e3e34e3eee389c8b60a9996f39
Ghazian Gunawan <112075463+ghaziangunawan@users.noreply.github.com>
2022-09-15 11:19:38 +0700

    update lab02

--- wishlist/views.py
+++ wishlist/views.py
@@ -1,11 +1,24 @@
 from django.shortcuts import render
 from wishlist.models import ItemWishlist
+from django.http import HttpResponse
+from django.core import serializers
 
 # Create your views here.
 def show_wishlist(request):
     data_wishlist_item = ItemWishlist.objects.all()
     context = {
     'list_item': data_wishlist_item,
-    'name': 'Cinoy'
+    'name': 'Muhammad Ghazian Gunawan'
     }
     return render(request, "wishlist.html",context)
+
+def show_xml(request):
+    data = ItemWishlist.objects.all()
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+def show_json_by_id(request,id):
+    data = ItemWishlist.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+    
+
+
+    
